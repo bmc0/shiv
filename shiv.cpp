fce14ebaa44ee549b31e3716da8e77cdcbc9ff3f
Michael Barbour <barbour.michael.0@gmail.com>
2016-06-08 01:01:40 -0400

    Use outlines instead of first inset for boundary crossing detection.

--- shiv.cpp
+++ shiv.cpp
@@ -181,7 +181,6 @@ struct island {
 	ClipperLib::Paths infill_insets;
 	ClipperLib::Paths solid_infill;
 	ClipperLib::Paths sparse_infill;
-	ClipperLib::Paths boundaries;
 	ClipperLib::Paths comb_boundaries;  /* simplified boundaries */
 	struct cint_rect box;  /* bounding box */
 };
@@ -880,9 +879,8 @@ static void generate_insets(struct slice *slice)
 		ClipperLib::CleanPolygons(island.infill_insets, CLEAN_DIST * 4.0);
 
 		done:
-		island.boundaries = (config.shells > 0) ? island.insets[0] : island.infill_insets;
 		if (config.comb) {
-			island.comb_boundaries = island.boundaries;
+			island.comb_boundaries = (config.shells > 0) ? island.insets[0] : island.infill_insets;
 			ClipperLib::CleanPolygons(island.comb_boundaries, (ClipperLib::cInt) MAXIMUM(config.extrusion_width * 0.25 * SCALE_CONSTANT, CLEAN_DIST * 4.0));
 		}
 		if (config.shells > 1 && config.fill_inset_gaps) {
@@ -1294,7 +1292,7 @@ static bool crosses_boundary(struct machine *m, struct island *island, ClipperLi
 {
 	ClipperLib::IntPoint p0(m->x, m->y);
 	ClipperLib::IntPoint p1(x, y);
-	for (ClipperLib::Path &p : island->boundaries) {
+	for (ClipperLib::Path &p : island->outlines) {
 		if (get_boundary_crossing(p, p0, p1) >= 0)
 			return true;
 	}
