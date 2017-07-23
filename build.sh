c1b71732de3ce52cbee0534abd7662955a7a7f47
Michael Barbour <barbour.michael.0@gmail.com>
2017-07-23 18:41:19 -0400

    build.sh: Use $CPPFLAGS.

--- build.sh
+++ build.sh
@@ -9,6 +9,6 @@ fail() {
 CXXFLAGS="-O2 -std=gnu++11 -Wall -Wextra -fopenmp $CXXFLAGS"
 LDFLAGS="-lm $LDFLAGS"
 
-$CXX -o shiv $CXXFLAGS $LDFLAGS shiv.cpp clipper.cpp || fail $?
+$CXX -o shiv $CXXFLAGS $LDFLAGS $CPPFLAGS shiv.cpp clipper.cpp || fail $?
 
 echo ">> Build successful"
